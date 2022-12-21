2c67f7420989d7363ca2853de5577328d5f8e439
HilalVildan <hilalsarioglu96@gmail.com>
2022-12-21 13:56:35 +0100

    .

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+import React from "react";
 
 const Header = () => {
   return (
-    <div className='header-div'>
-    <h2 className='baslik'>POPULER TOUR PLACES</h2>
+    <div className="header-div">
+      <h2 className="main-header">POPULER TOUR PLACES</h2>
     </div>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
